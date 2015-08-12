9b89ca7aaec2924a6ff47b8267fa87198f68e141
Peter Polidoro <peterpolidoro@gmail.com>
2015-08-12 11:00:32 -0400

    Updated homepage url.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/janelia-modular-devices/modular_device_python',
+    url='https://github.com/janelia-pypi/modular_device_python',
 
     # Author details
     author='Peter Polidoro',
